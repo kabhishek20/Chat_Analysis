699dd237cbb07d65896105fbf0742c30004c37e8
Kumar Abhishek <82359053+kabhishek20@users.noreply.github.com>
2023-06-19 21:33:34 +0530

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -76,7 +76,8 @@ if __name__=='__main__':
         # st.dataframe(df)
 
         user_list = df['user'].unique().tolist()
-        user_list.remove('group_notification')
+        if "group_notification" in user_list:
+            user_list.remove('group_notification')
         user_list.sort()
         user_list.insert(0,"Overall")
         selected_user = st.sidebar.selectbox("Select Username",user_list)
